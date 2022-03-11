9c8d2b88dfe226c2b78650e6a6180d9b569ddd68
Tomas Hellman <tomas.hellman@sr.se>
2022-03-11 13:26:45 +0100

    update

--- 06_Filer/ex04a_mata_in_tal/src/main.rs
+++ 06_Filer/ex04a_mata_in_tal/src/main.rs
@@ -3,14 +3,14 @@ use std::io::prelude::*;
 
 fn main() -> Result<(), std::io::Error> {
     print!("Skriv ett tal: ");
-    std::io::stdout().flush()?;
-    for rad in std::io::stdin().lock().lines() {
+    std::io::stdout().flush()?;	// question mark returns if error occur
+    for rad in std::io::stdin().lock().lines() {	// lock - lock to the current thread
 	if let Ok(n) = rad?.parse::<usize>() {
 	    println!("Du skrev {}", n);
 	    break;
 	}
 	print!("Ej tal, försök igen: ");
-	std::io::stdout().flush()?;
+	std::io::stdout().flush()?; // question mark returns if error occur
     }
     Ok(())
 }
